7fbb04e0b2e571fdbf6f69b234fb211afcf1f8f7
randyma01 <randyma01@gmail.com>
2019-10-15 13:58:37 -0600

    mv, mkdir for passengers query

---
+++ db/querys/passengersQuery.js
@@ -0,0 +1,11 @@
+/* 1. Registrar información de paciente (CRUD) */
+
+/* 2. Compra de boletos */
+
+/* 3. Check-in */
+
+/* 4. Ejecutar los siguientes reportes:
+  Vuelos asociados al pasajero: se muestra un listado con la información
+  de los vuelos para los que un pasajero ha adquirido boletos. Se puede 
+  filtrar por rango de fechas, por estado del vuelo. Para cada vuelo se 
+  debe mostrar el detalle correspondiente. */
