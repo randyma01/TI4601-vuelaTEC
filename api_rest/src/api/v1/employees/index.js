269e485fea5dc374f6a07dd30067eeb257ce438f
randyma01 <randyma01@gmail.com>
2019-10-18 21:02:21 -0600

    adding methods

--- api_rest/src/api/v1/employees/index.js
+++ api_rest/src/api/v1/employees/index.js
@@ -21,6 +21,44 @@ function EmployeesRoutes(server) {
           return reply.response(error).code(500);
         }
       }
+    },
+
+    // 1. verify check-in
+    {
+      method: 'GET',
+      path: '/employee/checkin/{id}',
+      handler: async (request, reply) => {
+        try {
+          const personId = request.params.id;
+          const person = await Passengers.find();
+          return reply.response(person);
+        } catch (error) {
+          return reply.response(error).code(500);
+        }
+      }
+    },
+
+    // 2. passengers info
+    {
+      method: 'GET',
+      path: '/employee/passengersInfo/',
+      handler: async (request, reply) => {
+        try {
+          const person = await Passengers.aggregate([
+            {
+              $lookup: {
+                from: 'tickets',
+                localField: '_id',
+                foreignField: 'passenger_id',
+                as: 'ticketsInfo'
+              }
+            }
+          ]).pretty();
+          return reply.response(person);
+        } catch (error) {
+          return reply.response(error).code(500);
+        }
+      }
     }
   ]);
 }
